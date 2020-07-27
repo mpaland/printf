24710ad7f495ee5c9861fc9a95b847bb33819526
Andrey Korol <andreykorol@hotmail.com>
2020-07-27 11:32:09 +0300

    added vfctprintf() support

--- printf.c
+++ printf.c
@@ -912,3 +912,10 @@ int fctprintf(void (*out)(char character, void* arg), void* arg, const char* for
   va_end(va);
   return ret;
 }
+
+int vfctprintf(void (*out)(char character, void* arg), void* arg, const char* format, va_list va)
+{
+  const out_fct_wrap_type out_fct_wrap = { out, arg };
+  const int ret = _vsnprintf(_out_fct, (char*)(uintptr_t)&out_fct_wrap, (size_t)-1, format, va);
+  return ret;
+}
