c6d5acc79858466ab3ca228ad225f5af108228c1
Eyal Rozenberg <eyalroz1@gmx.com>
2021-08-13 15:15:34 +0300

    Added another rounding-related check to the test suite.

--- test/test_suite.cpp
+++ test/test_suite.cpp
@@ -834,10 +834,10 @@ TEST_CASE("float", "[]" ) {
   PRINTING_CHECK("-0",               ==, test::sprintf_, buffer, "%+g", -0.);
   PRINTING_CHECK("-4e+04",           ==, test::sprintf_, buffer, "%.1g", -40661.5);
   PRINTING_CHECK("-4.e+04",          ==, test::sprintf_, buffer, "%#.1g", -40661.5);
+  // Rounding-focused checks
   PRINTING_CHECK("4.895512e+04",     ==, test::sprintf_, buffer, "%e", 48955.125);
-  // This checks the "banker's rounding" behavior
   PRINTING_CHECK("9.2524e+04",       ==, test::sprintf_, buffer, "%.4e", 92523.5);
-
+  PRINTING_CHECK("-8.380923438e+04", ==, test::sprintf_, buffer, "%.9e", -83809.234375);
 #endif
 
   // out of range for float: should switch to exp notation if supported, else empty
